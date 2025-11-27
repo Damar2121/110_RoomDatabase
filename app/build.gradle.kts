af784fc1dedaab4953b5457c7bbdb3a437d48bf5
Damar2121 <damar.vivoy51@gmail.com>
2025-11-27 15:42:56 +0700

    menambah dependencies untuk room baru di buld.gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,9 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.compose.icons)
+    //Room
+    implementation(libs.bundles.room)
+    ksp(libs.room.compiler)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
