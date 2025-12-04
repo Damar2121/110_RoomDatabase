386a0dbd4a2dfc2fabe8ad80f62df5caefc1d6b5
Damar2121 <damar.vivoy51@gmail.com>
2025-12-04 18:02:44 +0700

    menambah update di SiswaDao

--- app/src/main/java/com/example/pertemuan9/room/SiswaDao.kt
+++ app/src/main/java/com/example/pertemuan9/room/SiswaDao.kt
@@ -5,6 +5,7 @@ import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Update
 import kotlinx.coroutines.flow.Flow
 
 @Dao
@@ -17,6 +18,9 @@ interface SiswaDao {
 
     @Query("SELECT * from tblSiswa WHERE id = :id")
     fun getSiswa(id: Int): Flow<Siswa>
+
+    @Update
+    suspend fun update(siswa: Siswa)
 
     @Delete
     suspend fun delete(siswa: Siswa)
